8e92357ccbbe509c8cbaa02a9888ea516c59511b
Lina Zerkly <zerkly.lina@gmail.com>
2023-05-31 14:24:26 +0200

    handlebars and views

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
-app.use(express.static('public')); // Make everything inside of public/ available
+app.use(express.static("public")); // Make everything inside of public/ available
 
-// app.get(path, code);
-// app.get(path, (req, res, next) => {});
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials"); //tell HBS which directory we use for partials
 
 // GET /
 app.get("/", (req, res, next) => {
-    console.log("we have received a request for the HOMEPAGE");
-    res.sendFile(__dirname + "/views/home-page.html")
+  console.log("we have received a request for the HOMEPAGE");
+  res.render("home-page");
 });
 
-
 app.get("/contact", (req, res, next) => {
-    res.sendFile(__dirname + "/views/contact-page.html")
+  res.render("contact-page");
 });
 
 app.listen(3000, () => {
   console.log("server listening on port 3000...");
+});
+
+app.get("/pizza/margarita", (req, res, next) => {
+  const info = {
+    name: "Margarita",
+    price: 15,
+    image: (src = "/images/margarita.jpg"),
+  };
+
+  res.render("product", info);
+});
+
+app.get("/pizza/veggie", (req, res, next) => {
+  const info = {
+      name: 'Veggie Pizza',
+      recommendedDrink: 'power smoothie',
+      ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+      image: (src = "/images/veggie.jpg"),
+    };
+  res.render("product", info);
+});
+
+app.get("/pizza/seafood", (req, res, next) => {
+  const info = {
+    name: "Seafood",
+    price: 30,
+    image: (src = "/images/seafood.jpg"),
+  };
+  res.render("product", info);
 });
